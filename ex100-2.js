89efe0590f32ac94bce4afe3eb7f18e1677249a1
kyosukeota <zm18242@josai.ac.jp>
2021-02-01 10:32:40 +0000

    2/1

--- ex100-2.js
+++ ex100-2.js
@@ -1,4 +1,4 @@
 function imgchange1(){document.getElementById('logo').src='./image/DSC_0846.JPG';}
 function imgchange2(){document.getElementById('logo').src='./image/soccer_dribble2.png';}
 function imgchange3(){document.getElementById('logo').src='./image/drink_sake.png';}
-function imgchange4(){document.getElementById('logo').src='./image/sweets_manju.png';}
+function imgchange4(){document.getElementById('logo').src='./image/IMG_5233.JPG';}
